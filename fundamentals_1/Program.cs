850f1439aa2b9be654b7e7b292c512bc3d252d43
Shawn Baugh II <shawnbaughcreative@gmail.com>
2017-07-31 16:49:26 -0500

    Finished assignment.

--- fundamentals_1/Program.cs
+++ fundamentals_1/Program.cs
@@ -6,7 +6,32 @@ namespace fundamentals_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 1-255
+            for (int i = 1;i < 256;i++)
+            {
+                Console.WriteLine(i);
+            }
+            // 1-1000 divisible by 3 and 5
+            for (int i = 0; i < 1001; i++)
+            {
+                if (i % 3 == 0 || i % 5 ==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            // Fizz for 3, Buzz for 5
+            for (int i = 0; i < 1001; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+            }
+
         }
     }
 }
